3dbb45c905a8b1b264917beb2d731eb3f8274f62
Uzi0603 <preposterous@163.com>
2021-04-28 15:40:13 +0800

    4-28

---
+++ hw/week9/A配平化学方程式.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+typedef long long LL;
+const int inf=~(1u<<31u);
+const LL linf=~(1LLu<<63u);
+
+using namespace std;
+
+//2019-12 T3
+int main(){
+    
+    return 0;
+}
