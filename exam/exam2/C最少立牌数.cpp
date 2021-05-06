a958a8b42ea17cf06399392ab863022c7642a08e
Uzi0603 <preposterous@163.com>
2021-05-06 23:01:56 +0800

    5.6

--- exam/exam2/C最少立牌数.cpp
+++ exam/exam2/C最少立牌数.cpp
@@ -1,16 +1,34 @@
 #include<bits/stdc++.h>
 
-#define mem(f,v) memset(f,v,sizeof(f))
+#define mem(f, v) memset(f,v,sizeof(f))
 
 typedef long long LL;
-const int inf=~(1u<<31u);
-const LL linf=~(1LLu<<63u);
+const int inf = ~(1u << 31u);
+const LL linf = ~(1LLu << 63u);
 
 using namespace std;
 
 //csp第二次模测 c题
-int main(){
+int n, k, res = 0;
+int person[1000010], day[1000010];
+
+int main() {
     ios::sync_with_stdio(false);
-    
+    mem(person, 0);
+    mem(day, 0);
+    cin >> n;
+    while (n--) {
+        cin >> k;
+        int last = 0;
+        for (int i = 1; i <= k; i++) {
+            cin >> day[i];
+            last = max(person[day[i]], last);
+        }
+        last++;
+        res = max(res, last);
+        for (int i = 1; i <= k; i++)
+            person[day[i]] = last;
+    }
+    cout << res << endl;
     return 0;
 }
