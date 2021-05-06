64737ddb13079b3e48010f42969a703eeb9ed75d
Uzi0603 <preposterous@163.com>
2021-05-06 22:08:53 +0800

    5-6

--- T3/T3模测二.cpp
+++ T3/T3模测二.cpp
@@ -2,15 +2,17 @@
 
 #define mem(f,v) memset(f,v,sizeof(f))
 
-typedef long long LL;
-const int inf=~(1u<<31u);
+typedef long long LL;const int inf=~(1u<<31u);
 const LL linf=~(1LLu<<63u);
 
 using namespace std;
 
-//csp第二次模测 c题
+//第二次T3测试
+
+
+
 int main(){
     ios::sync_with_stdio(false);
-    
+
     return 0;
 }
