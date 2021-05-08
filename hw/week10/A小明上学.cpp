b2f36ea0167c8efcab6ef610513cdbcfc0670d44
Uzi0603 <preposterous@163.com>
2021-05-09 00:12:56 +0800

    1

---
+++ hw/week10/A小明上学.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+#define mem(f,v) memset(f,v,sizeof(f))
+
+typedef long long ll;
+const int inf=~(1u<<31u);
+const ll linf=~(1LLu<<63u);
+
+using namespace std;
+
+//第十周作业 A csp原题
+int main(){
+    ios::sync_with_stdio(false);
+
+    return 0;
+}
