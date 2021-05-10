e507e8d21cdedb02d3dfb4960e61a3605c6c5a24
Uzi0603 <preposterous@163.com>
2021-05-11 00:04:57 +0800

    5.11,cout关同步后别用endl

--- hw/week10/D逆序对个数.cpp
+++ hw/week10/D逆序对个数.cpp
@@ -10,13 +10,15 @@ const ll linf = ~(1LLu << 63u);
 using namespace std;
 
 //第十周作业 D 求逆序对个数
+
 #define lb(x) (x & (-x))
 
-int n, times, memory;
-int arr[maxn], BIT[maxn], ls[maxn], rs[maxn];
+int n;
+int BIT[maxn], res[maxn];
+pair<int,int> arr[maxn];
 
 void update(int x, int v) {
-    for (int i = x; i <= n; i += lb(i))
+    for (int i = x; i <= maxn; i += lb(i))//! 这里的范围是y的范围，和n没关系，必须是maxn
         BIT[i] += v;
 }
 
@@ -29,27 +31,19 @@ int ask(int x) {
 
 int main() {
     ios::sync_with_stdio(false);
-    cin >> n;
+    cin>>n;
     for (int i = 1; i <= n; i++) {
-        cin >> times >> memory;
-        arr[i] = times + memory;
+        cin>>arr[i].first>>arr[i].second;
+        arr[i].first++,arr[i].second++;//避开为0的情况
     }
-    //正序找左边小过arr[i]的个数
-    mem(BIT, 0);
-    for (int i = 1; i <= n; i++) {
-        ls[i] = ask(arr[i] - 1);
-        update(arr[i], 1);
+    sort(arr+1,arr+n+1);
+    mem(BIT,0);
+    mem(res,0);
+    for(int i=1;i<=n;i++){
+        res[ask(arr[i].second)]++;
+        update(arr[i].second,1);
     }
-
-    //逆序找右边小过arr[i]的个数
-    mem(BIT, 0);
-    for (int i = n; i >= 1; i++) {
-        rs[i] = ask(arr[i] - 1);
-        update(arr[i], 1);
-    }
-
-    for (int i = 1; i <= n; i++)
-        cout << ls[i] << ' ' << rs[i] << endl;
-
+    for(int i=0;i<n;i++)
+        cout<<res[i]<<'\n';//! cout不要用endl，否则相当于没关同步
     return 0;
 }
