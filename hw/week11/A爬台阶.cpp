5c74d10295263d991190e1a36de1287262b02d6c
Uzi0603 <preposterous@163.com>
2021-05-14 18:17:24 +0800

    新建文件夹-.-

---
+++ hw/week11/A爬台阶.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+#define mem(f,v) memset(f,v,sizeof(f))
+
+typedef long long ll;
+const int maxn=100010;
+const int inf=~(1u<<31u);
+const ll linf=~(1LLu<<63u);
+
+using namespace std;
+
+//第十一周作业 A dp
+int main(){
+    ios::sync_with_stdio(false);
+
+    return 0;
+}
