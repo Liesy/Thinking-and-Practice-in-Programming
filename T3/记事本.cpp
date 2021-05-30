b3a27a70f866016f2da5b896abe6fbc00e2e98aa
Uzi0603 <preposterous@163.com>
2021-05-30 14:59:33 +0800

    5.30

--- T3/记事本.cpp
+++ T3/记事本.cpp
@@ -1,207 +1,336 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
-#define mem(f, v) memset(f,v,sizeof(f))
+#define mem(f, v) memset(f, v, sizeof(f))
 
 typedef long long ll;
-const int maxn = 5010;
-const int inf = ~(1u << 31u);
-const ll linf = ~(1LLu << 63u);
+const int maxn = 100010;
+const int inf = 0x3f3f3f3f;
+const ll linf = 0x3f3f3f3f3f3f3f3fll;
 
 using namespace std;
 
 //五月T3 记事本
 int n;
-char ch;
-string option1, option2, str;
-
-struct Cursor {
-    int row;
-    int column;
-
-    Cursor() { row = 1, column = 0; }
-};
-
-struct paste {//粘贴板
-    Cursor start;
-    Cursor over;
-
-    paste() {
-        start.row = 1, start.column = 0;
-        over.row = 1, over.column = 0;
-    }
-};
-
-string txt[maxn];//记事本
-int end_index = 1;//最后一行的索引
-Cursor cursorNow;//当前光标位置
-Cursor cursorRec;//记录点的光标位置
-bool shift = false;//粘滞
-
-bool atEnd(Cursor cur) {
-    if (cur.column == txt[cur.row].size())
-        return true;
-    return false;
-}
+int cnt = 0, paste_cnt = 0;
+vector<string> txt;
+vector<string> paste;//粘贴板
+pair<int, int> cursor = make_pair(1, 1);
+pair<int, int> cursor_begin;
+pair<int, int> cursor_end;
+bool shift_on = false;//是否开启了shift
+bool selected = false;//是否有字段被选中
 
 void move() {
-    if (option2 == "Home") {//移至当前行的开头
-        cursorNow.column = 0;
-        return;
+    selected = false;//光表进行移动出选中状态
+    string opt;
+    cin >> opt;
+    if (opt == "Home") { cursor.second = 1; }
+    else if (opt == "End") { cursor.second = (int) txt[cursor.first - 1].size() + 1; }
+    else if (opt == "Up") {
+        if (cursor.first == 1) return;
+        int up_len = (int) txt[cursor.first - 2].size();
+        if (up_len + 1 < cursor.second) {
+            cursor.first--;
+            cursor.second = up_len + 1;
+        } else cursor.first--;
+    } else if (opt == "Down") {
+        if (cursor.first == txt.size()) return;
+        int down_len = (int) txt[cursor.first].size();
+        if (down_len + 1 < cursor.second) {
+            cursor.first++;
+            cursor.second = down_len + 1;
+        } else cursor.first++;
+    } else if (opt == "Left") {
+        if (cursor.first == 1 && cursor.second == 1) return;
+        else if (cursor.second == 1) {
+            cursor.first--;
+            cursor.second = (int) txt[cursor.first - 1].size() + 1;
+        } else cursor.second--;
+    } else if (opt == "Right") {
+        if (cursor.first == txt.size() && cursor.second == txt[cursor.first - 1].size() + 1)
+            return;
+        else if (cursor.second == txt[cursor.first - 1].size() + 1) {
+            cursor.first++;
+            cursor.second = 1;
+        } else cursor.second++;
     }
-    if (option2 == "End") {//移至当前行的末尾
-        cursorNow.column = txt[cursorNow.row].size();
-        return;
+}
+
+void backspace() {
+    if (cursor.first == 1 && cursor.second == 1) return;//在文件开头
+    else if (cursor.second == 1) {//在一行的开头
+        cursor.second = txt[cursor.first - 2].size() + 1;//光标位置更新
+        //合并
+        string a = txt[cursor.first - 2] + txt[cursor.first - 1];
+        txt[cursor.first - 2] = a;
+        txt.erase(txt.begin() + cursor.first - 1, txt.begin() + cursor.first);
+        cursor.first--;//光标位置更新
+    } else {
+        if (txt[cursor.first - 1][cursor.second - 2] != ' ') cnt--; //删除一个字符
+        txt[cursor.first - 1].erase(cursor.second - 2, 1);
+        cursor.second--;//光标位置前进
     }
-    if (option2 == "Up") {
-        if (cursorNow.row == 1)
-            return;
-        cursorNow.row--;
-        if (txt[cursorNow.row].size() < cursorNow.column)
-            cursorNow.column = txt[cursorNow.row].size();
-        return;
+}
+
+void clearSelected() {
+    selected = false;
+    if (cursor_begin.first > cursor.first ||
+        (cursor_begin.first == cursor.first && cursor_begin.second >= cursor.second)) {
+        swap(cursor_begin.first, cursor.first);
+        swap(cursor_begin.second, cursor.second);
     }
-    if (option2 == "Down") {
-        if (cursorNow.row >= end_index)
-            return;
-        cursorNow.row++;
-        if (txt[cursorNow.row].size() < cursorNow.column)
-            cursorNow.column = txt[cursorNow.row].size();
-        return;
-    }
-    if (option2 == "Left") {
-        if (cursorNow.row == 1 && cursorNow.column == 0)
-            return;
-        if (cursorNow.column == 0) {
-            cursorNow.row--;
-            cursorNow.column = txt[cursorNow.row].size();
-            return;
-        }
-        cursorNow.column--;
-        return;
-    }
-    if (option2 == "Right") {
-        if (cursorNow.row >= end_index && atEnd(cursorNow))
-            return;
-        if (atEnd(cursorNow)) {
-            cursorNow.row++;
-            cursorNow.column = 0;
-            return;
-        }
-        cursorNow.column++;
-        return;
+    while (cursor_begin != cursor) {
+        backspace();
     }
 }
 
 void insert() {
-    if (option2 == "Char") {
-        if (cursorNow.row > end_index)
-            end_index = cursorNow.row;
-        if (atEnd(cursorNow))
-            txt[cursorNow.row].push_back(ch);
-        else
-            txt[cursorNow.row].insert(cursorNow.column, 1, ch);
-        cursorNow.column++;
-        return;
-    }
-    if (option2 == "Enter") {
-        if (cursorNow.row >= end_index && atEnd(cursorNow)) {//在文章末尾
-            cursorNow.row++;
-            cursorNow.column = 0;
-            return;
+    string opt;
+    cin >> opt;
+    if (opt == "Char") {
+        if (selected) clearSelected();//如果被选中了，清空被选中的区域
+        selected = false;//退出选中状态
+
+        string c;
+        cin >> c;
+        txt[(int) cursor.first - 1].insert(cursor.second - 1, c, 0, 1);
+        cursor.second++;
+        cnt++;
+    } else if (opt == "Enter") {
+        if (selected) clearSelected();//如果被选中了，清空被选中的区域
+        selected = false;//退出选中状态
+
+        if (cursor.second == txt[cursor.first - 1].size() + 1) {//在一行的结尾换行
+            txt.insert(txt.begin() + cursor.first, string());
+            cursor.first++;
+            cursor.second = 1;
+        } else {
+            //不是在一行的结尾换行
+            string a(txt[cursor.first - 1], 0, cursor.second - 1);
+            string b(txt[cursor.first - 1], cursor.second - 1, txt[cursor.first - 1].size() - cursor.second + 1);
+            txt[cursor.first - 1] = a;
+            txt.insert(txt.begin() + cursor.first, b);
+            cursor.first++;
+            cursor.second = 1;
         }
-        //全部后移
-        end_index++;
-        for (int i = end_index - 1; i >= cursorNow.row + 1; i--) txt[i + 1] = txt[i];
-        txt[cursorNow.row + 1].clear();
-        if (!atEnd(cursorNow)) {
-            //截断
-            str = txt[cursorNow.row].substr(cursorNow.column);
-            //换行
-            txt[cursorNow.row + 1] = str;
-            //同
-            str = txt[cursorNow.row].substr(0, cursorNow.column);
-            txt[cursorNow.row] = str;
+    } else if (opt == "Space") {
+        if (selected) clearSelected();//如果被选中了，清空被选中的区域
+        selected = false;//退出选中状态
+
+        string str = " ";
+        txt[cursor.first - 1].insert(cursor.second - 1, str, 0, 1);
+        cursor.second++;
+    } else if (opt == "Paste") {
+        if (paste.empty()) return;
+        //剪切板不为空
+        if (selected) clearSelected();//退出选中状态
+        selected = false;
+        string a(txt[cursor.first - 1], 0, cursor.second - 1);
+        string b(txt[cursor.first - 1], cursor.second - 1, txt[cursor.first - 1].size() - cursor.second + 1);
+        a += paste[0];
+        txt[cursor.first - 1] = a;
+        //处理n-1行
+        for (int k = 1; k < paste.size(); k++) {
+            cursor.first++;
+            txt.insert(txt.begin() + cursor.first - 1, paste[k]);
         }
-        cursorNow.row++, cursorNow.column = 0;
-        return;
-    }
-    if (option2 == "Space") {
-        if (cursorNow.row > end_index)
-            end_index = cursorNow.row;
-        if (atEnd(cursorNow))
-            txt[cursorNow.row].push_back(' ');
-        else
-            txt[cursorNow.row].insert(cursorNow.column, 1, ' ');
-        cursorNow.column++;
-        return;
-    }
-    if (option2 == "Paste") {
-        return;
+        cursor.second = txt[cursor.first - 1].size() + 1;
+        //处理最后一行
+        txt[cursor.first - 1] += b;
+        cnt += paste_cnt;
     }
 }
 
 void remove() {
-    if (option2 == "Del") {
-        if (atEnd(cursorNow)) {
-            if (cursorNow.row >= end_index)
-                return;
-            txt[cursorNow.row] += txt[cursorNow.row + 1];
-            end_index--;
-            for (int i = cursorNow.row + 1; i <= end_index; i++) txt[i] = txt[i + 1];
-            txt[end_index + 1].clear();
+    string opt;
+    cin >> opt;
+    if (!selected && opt == "Del") {
+        //当前在文件的末尾位置
+        if (cursor.first == txt.size() && cursor.second == txt[cursor.first - 1].size() + 1)
             return;
+        else if (cursor.second == txt[cursor.first - 1].size() + 1) {
+            //将两行合并,光标位置不更新
+            string a = txt[cursor.first - 1] + txt[cursor.first];
+            txt[cursor.first - 1] = a;
+            txt.erase(txt.begin() + cursor.first, txt.begin() + cursor.first + 1);
+        } else {
+            //删除一个字符,光标位置不变
+            if (txt[cursor.first - 1][cursor.second - 1] != ' ') cnt--;
+            txt[cursor.first - 1].erase(cursor.second - 1, 1);
         }
-        txt[cursorNow.row].erase(cursorNow.column, 1);
-        return;
+    } else if (!selected && opt == "Backspace") {
+        backspace();
+    } else if (selected) {
+        clearSelected();
     }
-    if (option2 == "Backspace") {
-        if (cursorNow.row > end_index && atEnd(cursorNow)) {//在文章末尾
-            cursorNow.row--;
-            if (cursorNow.row == end_index)
-                cursorNow.column = txt[cursorNow.row].size();
-            return;
+}
+
+void shift() {
+    if (!shift_on && !selected) {//如果之前没有开启shift,也没有被选中，将开启shift,
+        shift_on = true;
+        selected = false;//还没有区域被选中
+        //当前位置为记录点
+        cursor_begin.first = cursor.first;
+        cursor_begin.second = cursor.second;
+    } else if (shift_on) {//之前已经开启shift了，将进入选中状态
+        shift_on = false;
+        selected = true;
+        cursor_end.first = cursor.first;
+        cursor_end.second = cursor.second;
+        if (cursor_begin == cursor)//光标没动
+            selected = false;
+    } else if (!shift_on && selected) {
+        //在选中状态下进入粘滞状态
+        //保留上次的记录点为当前的记录点
+        shift_on = true;
+        selected = false;
+    }
+}
+
+void find() {
+    string str;
+    cin >> str;
+    int cnt_cmp = 0;
+    if (!selected) {
+        for (auto & temp : txt)
+            for (int j = 0; j < temp.size(); j++) {
+                if (temp.size() - j < str.size()) break;
+                if (temp[j] == str[0]) {
+                    bool com = true;
+                    for (int k = 0; k < str.size(); k++)
+                        if (temp[j + k] != str[k]) {
+                            com = false;
+                            break;
+                        }
+                    if (com)
+                        cnt_cmp++;
+                }
+            }
+        cout << cnt_cmp << '\n';
+    } else {
+        pair<int, int> pos = cursor_begin;
+        pair<int, int> pos_end = cursor;
+        if (pos > pos_end)
+            swap(pos, pos_end);
+        int i = pos.first - 1;
+        int j = pos.second - 1;
+        for (; i < pos_end.first - 1; i++) {
+            for (; j < txt[i].size(); j++) {
+                if (txt[i].size() - j < str.size()) break;
+                if (txt[i][j] == str[0]) {
+                    bool com = true;
+                    for (int k = 0; k < str.size(); k++)
+                        if (txt[i][j + k] != str[k]) {
+                            com = false;
+                            break;
+                        }
+                    if (com)
+                        cnt_cmp++;
+                }
+            }
+            j = 0;
         }
-        if (cursorNow.column == 0) {
-            if (cursorNow.row == 1)
-                return;
-            cursorNow.row--;
-            cursorNow.column = txt[cursorNow.row].size();
-            txt[cursorNow.row] += txt[cursorNow.row + 1];
-            end_index--;
-            for (int i = cursorNow.row + 1; i <= end_index; i++) txt[i] = txt[i + 1];
-            txt[end_index + 1].clear();
-            return;
+        for (j = 0; j < pos_end.second - 1; j++) {
+            if (txt[i].size() - j < str.size()) break;
+            if (txt[i][j] == str[0]) {
+                bool com = true;
+                for (int k = 0; k < str.size(); k++)
+                    if (txt[i][j + k] != str[k]) {
+                        com = false;
+                        break;
+                    }
+                if (com)
+                    cnt_cmp++;
+            }
         }
-        txt[cursorNow.row].erase(cursorNow.column - 1, 1);
-        cursorNow.column--;
-        return;
+        cout << cnt_cmp << '\n';
+    }
+}
+
+void count() {
+    if (!selected) {
+        cout << cnt << endl;
+    } else {
+        int cnt_a = 0;
+        pair<int, int> pos = cursor_begin;
+        pair<int, int> pos_end = cursor;
+        if (pos > pos_end)
+            swap(pos, pos_end);
+        int i = pos.first - 1;
+        int j = pos.second - 1;
+        for (; i < pos_end.first - 1; i++) {
+            for (; j < txt[i].size(); j++) {
+                if (txt[i][j] != ' ')
+                    cnt_a++;
+            }
+            j = 0;
+        }
+        for (; j < pos_end.second - 1; j++)
+            if (txt[i][j] != ' ')
+                cnt_a++;
+        cout << cnt_a << '\n';
+    }
+}
+
+void copy() {
+    if (!selected && !txt[cursor.first - 1].empty()) {
+        //当前行不为空
+        paste.clear();
+        paste.emplace_back(txt[cursor.first - 1]);
+        paste_cnt = 0;
+        for (char x : paste[0])
+            if (x != ' ') paste_cnt++;
+    } else if (selected) {
+        paste.clear();
+        //当标记点和光标出在同一行的时候
+        pair<int, int> pos = cursor_begin;
+        pair<int, int> pos_end = cursor;
+        if (pos > pos_end)
+            swap(pos, pos_end);
+        //当标记点和光标出在同一行的时候
+        if (pos.first == pos_end.first) { //被选中的字段为空？？怎么办
+            string a(txt[cursor.first - 1], pos.second - 1, pos_end.second - pos.second);
+            paste.push_back(a);
+        } else {
+            //不在同一行的时候
+            string a(txt[pos.first - 1], pos.second - 1, txt[pos.first - 1].size() - pos.second + 1);
+            string b(txt[pos_end.first - 1], 0, pos_end.second - 1);
+            paste.push_back(a);
+            pos.first++;
+            for (; pos.first < pos_end.first; pos.first++)
+                paste.push_back(txt[pos.first - 1]);
+            paste.push_back(b);
+        }
+        paste_cnt = 0;
+        for (auto & temp : paste)
+            for (int j = 0; j < temp.size(); j++)
+                if (temp[j] != ' ') paste_cnt++;
     }
 }
 
 int main() {
-    ios::sync_with_stdio(false);
     cin >> n;
-    while (n--) {
-        cin >> option1;
-        if (option1 == "MOVE") {
-            cin >> option2;
+    txt.emplace_back(string());
+    string order;
+    for (int i = 0; i < n; i++) {
+        cin >> order;
+        if (order == "MOVE")
             move();
-        } else if (option1 == "INSERT") {
-            cin >> option2;
-            if (option2 == "Char")
-                cin >> ch;
+        else if (order == "INSERT")
             insert();
-        } else if (option1 == "REMOVE") {
-            cin >> option2;
+        else if (order == "REMOVE")
             remove();
-        } else if (option1 == "SHIFT") {
-        } else if (option1 == "FIND") {
-        } else if (option1 == "COUNT") {
-        } else if (option1 == "COPY") {
-        } else if (option1 == "PRINT") {
-            for (int i = 1; i <= end_index; i++)
-                cout << txt[i] << '\n';
-            cout << '\n';
+        else if (order == "SHIFT")
+            shift();
+        else if (order == "FIND")
+            find();
+        else if (order == "COUNT")
+            count();
+        else if (order == "COPY")
+            copy();
+        else if (order == "PRINT") {
+            for (int j = 0; j < txt.size(); j++)
+                cout << txt[j] << '\n';
         }
     }
     return 0;
