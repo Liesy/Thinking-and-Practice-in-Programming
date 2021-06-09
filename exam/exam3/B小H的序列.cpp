9dc450626e68995e36b0344008b729105442300f
Uzi0603 <preposterous@163.com>
2021-06-09 23:06:47 +0800

    6.9

--- exam/exam3/B小H的序列.cpp
+++ exam/exam3/B小H的序列.cpp
@@ -10,20 +10,32 @@ const ll linf = 0x3f3f3f3f3f3f3f3fll;
 using namespace std;
 
 int n, m, opt, x, y;
-int a[maxn];
+vector<int> a;
+
+void process(){
+    //去重
+    auto last = unique(a.begin(),a.end());
+    a.erase(last,a.end());
+}
 
 int main(){
     ios::sync_with_stdio(false);
     cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-        cin >> a[i];
+    for (int i = 1; i <= n; i++){
+        int temp;
+        cin >> temp;
+        a.emplace_back(temp);
+    }
+    process();
     while(m--) {
         cin >> opt;
         if (opt == 1){
             cin >> x >> y;
+            for (auto &num: a) if (num == x) num = y;
         }
         else if (opt == 2){
-
+            process();
+            cout << a.size() << '\n';
         }
     }
     return 0;
