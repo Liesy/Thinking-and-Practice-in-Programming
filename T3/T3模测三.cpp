1ab1f60d0cfe49d3fa741412ad2f2a206cd2eb93
Uzi0603 <preposterous@163.com>
2021-06-10 11:12:08 +0800

    6.10 wrong answer

--- T3/T3模测三.cpp
+++ T3/T3模测三.cpp
@@ -24,16 +24,34 @@ int n, m;
 
 /*   日常长跑分数
  *   YYYYMMDD 完成日期
+ *   p 记录的来源学号；
+ *   hh:mm:ss 开始时间
+ *   hh:mm:ss 结束时间
+ *   l 运动距离(float，精确到2位)
+ *   a'b" 总暂停时间
+ *   s 总步数
 */
+
+struct raceRecord{
+    string YYYYMMDD;
+    int start;
+    int end;
+    float l;
+    int pause;
+    int s;
+};
 
 // vector<pair<string,int>> stu;
 map<ll,int> stu;//学号-分数
+map<ll,char> stuGender;//学号-性别
+map<ll,int> stuCounts;//学号-到课次数
+map<ll,vector<raceRecord>> stuRace;//学号-日常长跑记录
 
 int raceScore(char gen, string ss){//返回期末长跑测试成绩
     int min, sec;
     int temp = 0;
     for (auto &x: ss){
-        if (x == 'c') min = temp, temp = 0;//遇到分隔符
+        if (x == '\'') min = temp, temp = 0;//遇到分隔符
         else temp = 10 * temp + x - '0';
     }
     sec = temp;
@@ -66,32 +84,102 @@ int raceScore(char gen, string ss){//返回期末长跑测试成绩
     }
 }
 
-int countScore(int cou){//返回到课次数对应成绩
-    if (cou >= 18) return 5;
-    else if (cou >= 15) return 4;
-    else if (cou >= 12) return 3;
-    else if (cou >= 9) return 2;
-    else if (cou >= 6) return 1;
-    else return 0;
+int split(string ss, char c){//返回时间字符串对应的总秒数
+    int res = 0, temp = 0;
+    for (auto &x: ss){
+        if (x == c || x == '\"') res += 60 * temp, temp = 0;//遇到分隔符
+        else temp = 10 * temp + x - '0';
+    }
+    res += temp;
+    return res;
+}
+
+bool judge(ll sp, raceRecord rr){//判断该条日常长跑记录是否合法
+    if ((stuGender[sp] == 'M' && rr.l < 3.0) || (stuGender[sp] == 'F' && rr.l < 1.5)) return false;
+    float v = (rr.l * 1000) / (rr.end - rr.start);
+    if (v > 5.0 || v < 2.0) return false;
+    if (rr.pause > 4 * 60 + 30) return false;
+    float step = (rr.l * 1000) / rr.s;
+    if (step > 1.5) return false;
+    if (stuRace[sp].empty()) return true;
+    auto r_pre = stuRace[sp].end();//最后一条合法记录
+    if (rr.YYYYMMDD != (*r_pre).YYYYMMDD) return true;
+    int gap = rr.start - (*r_pre).end;
+    if (gap < 6) return false;
+    return true;
+}
+
+void output(){
+    for (auto &x: stu){
+        cout << x.first << ' ' << x.second << ' ';
+        int score = x.second;
+        if (score >= 95) cout << "A";
+        else if (score >= 90) cout << "A-";
+        else if (score >= 85) cout << "B+";
+        else if (score >= 80) cout << "B";
+        else if (score >= 77) cout << "B-";
+        else if (score >= 73) cout << "C+";
+        else if (score >= 70) cout << "C";
+        else if (score >= 67) cout << "C-";
+        else if (score >= 63) cout << "D+";
+        else if (score >= 60) cout << "D";
+        else cout << "F";
+        cout << '\n';
+    }
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin >> n;
+    // freopen("1.in","r",stdin);
+    // freopen("1.out","w",stdout);
     for (int i = 1; i <= n; i++){
         ll p;
         char gender, flag;
         string race;
         int score, daily, counts, sumScore;
         cin >> p >> gender >> score >> race >> flag >> daily >> counts;
-        sumScore = score + raceScore(gender, race) + daily + countScore(counts);
+        stuGender.emplace(p, gender);
+        stuCounts.emplace(p, counts);
+        sumScore = score + raceScore(gender, race) + daily;
         if (flag == 'P') sumScore += 10;
         stu.emplace(p, sumScore);
     }
     cin >> m;
     for (int i = 1; i <= m; i++){
         ll p;
+        raceRecord r;
+        string t1, t2, t3;
+        cin >> r.YYYYMMDD >> p >> t1 >> t2 >> r.l >> t3 >> r.s;
+        r.start = split(t1, ':'), r.end = split(t2, ':');
+        r.pause = split(t3, '\'');
+        if (judge(p, r)) stuRace[p].emplace_back(r);
     }
+    //处理日常长跑分数
+    for (auto &x: stuRace){
+        if (x.second.empty()) continue;
+        int num = x.second.size();
+        stuCounts[x.first] += num;
+        if (num >= 21) stu[x.first] += 10;
+        else if (num >= 19) stu[x.first] += 9;
+        else if (num >= 17) stu[x.first] += 8;
+        else if (num >= 14) stu[x.first] += 7;
+        else if (num >= 11) stu[x.first] += 6;
+        else if (num >= 7) stu[x.first] += 4;
+        else if (num >= 3) stu[x.first] += 2;
+        else continue;
+    }
+    //处理出勤分数
+    for (auto &x: stuCounts){
+        int cou = x.second;
+        if (cou >= 18) stu[x.first] += 5;
+        else if (cou >= 15) stu[x.first] += 4;
+        else if (cou >= 12) stu[x.first] += 3;
+        else if (cou >= 9) stu[x.first] += 2;
+        else if (cou >= 6) stu[x.first] += 1;
+        else continue;
+    }
+    output();
     return 0;
 }
 
